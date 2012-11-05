14370e10f9aff439799c9c491466ff3efb3dd9d0
Austen Constable <austen@crossfire.ltd.uk>
2012-11-05 22:27:47 +0000

    Updated db migration plugin

--- src/main/db/create_schema_version.sql
+++ src/main/db/create_schema_version.sql
@@ -3,7 +3,7 @@ CREATE TABLE `schema_version` (
   `applied_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `duration` int(11) NOT NULL,
   UNIQUE KEY `version` (`version`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into schema_version (version, duration) values ('20110820130418', 0);
 insert into schema_version (version, duration) values ('20110820142226', 0);
